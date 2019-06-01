b8db79b6ce6ac371cc3da77f74d699163b284d7b
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-01 20:53:07 +0000

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,17 @@
+class CreateArtists < ActiveRecord::Migration [5.2]
+  def up
+  end
+
+  def down
+  end
+
+  def change
+    create_table :artists do |t|
+      binding.pry
+      t.string :name
+      t.string :genre
+      t.interger :age
+      t.string :hometown
+    end
+  end
+end
